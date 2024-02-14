53dde6b2ce5ea7aa3c3b444530cec2c2312f7bd4
sanskarsahu <sanskarsahu1580@gmail.com>
2024-02-14 13:59:49 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Navbar from "./Pages/Navbar";
+
+// Importing the components that i want to render for each route
+import Home from "./Pages/Home";
+import Blogs from "./Pages/Blogs";
+import Contact from "./Pages/Contact";
+
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar />
+      <nav>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/blog" element={<Blogs />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </nav>
     </div>
   );
 }
---
+++ src/Pages/Navbar.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      <a href="/">Home</a>
+      <a href="/blog">Blogs</a>
+      <a href="/contact">Contact</a>
+    </div>
+  );
+};
+export default Navbar;
